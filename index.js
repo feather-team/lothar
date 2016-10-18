c7d3a396ebba5d0a495b38e55e16f7aadef58b6c
zhanghao <jsyczhanghao@sina.com>
2016-10-18 15:36:44 +0800

    支持自定义标签

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ global.lothar = module.exports = require('feather2-lang');
 
 lothar.cli.name = 'lothar';
 lothar.cli.info = lothar.util.readJSON(__dirname + '/package.json');
+lothar.cli.version = function(){        
+    console.log(('Version: ' + feather.cli.info.version).bold.red);
+};
 lothar.require.prefixes.unshift('lothar');
 lothar.config.merge({
     server: {
@@ -11,6 +14,14 @@ lothar.config.merge({
         rewrite: 'index.php'
     }
 });
+lothar.config.set('template', {
+    suffix: 'html',
+    tags: {
+        raw: ['{!!', '!!}'],
+        content: ['{{', '}}'],
+        escapedContent: ['{{{', '}}}']
+    }
+})
 
 lothar.config.get('postprocessor').push('analyse');
 lothar.config.get('postpackager').push('position', 'runtime');
